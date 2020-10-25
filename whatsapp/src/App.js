53ebec8665e7a6802889ba40e9304aba821e1f15
caprioara <dragos.neatax@gmail.com>
2020-10-25 20:13:43 +0200

    sidebar chat

--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,10 +1,17 @@
 import React from "react"
 import './App.css';
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Lets build a WhatsApp clone!</h1>
+    // BEM naming convention
+    <div className="app">
+     {/* <h1>Lets build a WhatsApp clone!</h1> */}
+
+     <div className="app__body">
+       <Sidebar></Sidebar>
+       {/* Chat */}
+     </div>
     </div>
   );
 }
