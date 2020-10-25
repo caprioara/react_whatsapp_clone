bd69d4540a67713440fddf81ec97cc6bd952ecfd
caprioara <dragos.neatax@gmail.com>
2020-10-25 11:33:34 +0200

    cleaning code

---
+++ whatsapp/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAuRTwMaAFBmJpeulk_Z-HS_lniqP3TPp4",
+  authDomain: "whats-app-clone-76fb6.firebaseapp.com",
+  databaseURL: "https://whats-app-clone-76fb6.firebaseio.com",
+  projectId: "whats-app-clone-76fb6",
+  storageBucket: "whats-app-clone-76fb6.appspot.com",
+  messagingSenderId: "962899348249",
+  appId: "1:962899348249:web:b32a78dd3268dba03fd388",
+  measurementId: "G-TVY5GJ60E8"
+};
